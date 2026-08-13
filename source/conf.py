0d6561ef611cc1b289321433fdcfcd91fbb66b80
wanghanchao <whc@originqc.com>
2026-08-13 08:40:54 +0000

    [fix] fix sphinx config warnings in German branch
    Remove deprecated get_html_theme_path() call and fix html_static_path
    ('_static' does not exist; backslash path fixed to 'rst/images'),
    eliminating all build warnings. Aligns with other language branches.

--- source/conf.py
+++ source/conf.py
@@ -55,12 +55,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'rst\images']
+html_static_path = ['rst/images']
 
 # -- Options for PDF output --------------------------------------------------
 
