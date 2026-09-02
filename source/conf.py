9b064b8d3fb77d5af6f822c6b72d2a9f4ed009d4
wanghanchao <whc@originqc.com>
2026-09-02 06:47:15 +0000

    [docs] conf: dynamic PDF header version, cross-platform CJK font fallback for LaTeX (Windows fonts first, Noto CJK on Linux)

--- source/conf.py
+++ source/conf.py
@@ -158,10 +158,16 @@ latex_elements = {
     'preamble': r'''
 \usepackage{xeCJK}
 \usepackage{fancyhdr}
-% Use fonts available on Windows
+% CJK fonts: prefer Windows fonts (SimSun/SimHei/YaHei), fall back to Noto CJK on Linux
+\IfFontExistsTF{SimSun}{%
 \setCJKmainfont[BoldFont=SimHei, ItalicFont=KaiTi]{SimSun}
 \setCJKsansfont{SimHei}
 \setCJKmonofont{Microsoft YaHei}
+}{%
+\setCJKmainfont[BoldFont=Noto Sans CJK SC, ItalicFont=Noto Serif CJK SC]{Noto Serif CJK SC}
+\setCJKsansfont{Noto Sans CJK SC}
+\setCJKmonofont{Noto Sans CJK SC}
+}
 \XeTeXlinebreaklocale "zh"
 \XeTeXlinebreakskip = 0pt plus 1pt
 \parindent 2em
@@ -171,9 +177,9 @@ latex_elements = {
 \renewcommand\CJKfamilydefault{\CJKrmdefault}
 % Completely fix repeating section titles - force static content everywhere
 \fancyhf{}
-\renewcommand{\leftmark}{VQNET v2.18.0}
+\renewcommand{\leftmark}{VQNET @@RELEASE@@}
 \renewcommand{\rightmark}{}
 \fancyfoot[C]{\thepage}
 \pagestyle{fancy}
-'''
+'''.replace('@@RELEASE@@', release),
 }
